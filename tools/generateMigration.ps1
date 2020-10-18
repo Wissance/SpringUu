f074a53f044e69f7945f90a07e2a2680fb6bce80
Ushakov Michael <um.nix.user@gmail.com>
2020-10-18 23:55:02 +0500

    script develop

--- tools/generateMigration.ps1
+++ tools/generateMigration.ps1
@@ -1,4 +1,4 @@
-# Script for Automate migration generation with Liquibase
+# Script for Automate migration generation with Liquibase and Gradle!
 # You MUST Pass 2 parameters : 
 #    1) Migration number - integer value from 1 to +INFINITY
 #    2) Migration description - what was done, ONLY _ is ALLOWED to use as separator between words
@@ -10,9 +10,28 @@
 $migrationNumber = $args[0]
 $migrationName = $args[1]
 
+# TODO: UMV: Check Variables
+
+$gradleScriptName = "gradlew.bat"
+
 # 1. Process $migrationName replace ' ' with '_'
 $migrationName = $migrationName.Replace(' ', '_')
 # 2. Get current datetime
-$migrationFileName="Migration_$($migrationNumber)_$($migrationName).sql"
+$timestamp = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')
+$migrationFileName = "$($timestamp)_Migration_$($migrationNumber)_$($migrationName).sql"
 
 Write-Host "Generating migration name: $migrationFileName"
+
+$currentDir = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path
+# Here we assume that gradle is located in same dir as this script
+Write-Host "Working directory is: $currentDir"
+
+$gardleFullPath = Join-Path $currentDir $gradleScriptName
+Write-Host "Gradle script path is: $gardleFullPath"
+# 3. Remove generated files
+$migrationPath = Join-Path $currentDir "src\main\resources\db\changelog\"
+$liquibaseDiff = Join-Path $currentDir "diffOutputLog.txt"
+$liquibaseChangelog = Join-Path $migrationPath "newChangelog.xml"
+
+Write-Host "Liquibase diff file is: $liquibaseDiff"
+Write-Host "Liquibase changelog file is: $liquibaseChangelog"
