fdef519b95f1fb457c49c7c5801c68f78c143023
Ushakov Michael <um.nix.user@gmail.com>
2020-10-20 13:01:37 +0500

    Name strategy class was added

---
+++ src/com/wissance/utils/SnakePhysicalNamingStrategy.java
@@ -0,0 +1,36 @@
+package com.wissance.utils;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class SnakePhysicalNamingStrategy implements PhysicalNamingStrategy {
+ 
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+ 
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+ 
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+ 
+    @Override
+    public Identifier toPhysicalSequenceName(final Identifier identifier, JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+ 
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+        return toSnakeCase(identifier);
+    }
+ 
+    private Identifier toSnakeCase(Identifier identifier) {
+        return Identifier.toIdentifier(identifier.getText().replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase());
+    }
+}
